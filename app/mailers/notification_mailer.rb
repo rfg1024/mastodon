74d8e2fa0b0b708db7a804b1ff1f112870027250
rfg1024 <rfg1024@outlook.com>
2022-12-04 18:49:30 +0000

    change in notification mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -7,57 +7,61 @@ class NotificationMailer < ApplicationMailer
   helper RoutingHelper
 
   def mention(recipient, notification)
-    @me     = recipient
-    @status = notification.target_status
+    #@me     = recipient
+    #@status = notification.target_status
 
-    return unless @me.user.functional? && @status.present?
+    #return unless @me.user.functional? && @status.present?
 
-    locale_for_account(@me) do
-      thread_by_conversation(@status.conversation)
-      mail to: @me.user.email, subject: I18n.t('notification_mailer.mention.subject', name: @status.account.acct)
-    end
+    #locale_for_account(@me) do
+    #  thread_by_conversation(@status.conversation)
+    #  mail to: @me.user.email, subject: I18n.t('notification_mailer.mention.subject', name: @status.account.acct)
+    #end
+    return
   end
 
   def follow(recipient, notification)
-    @me      = recipient
-    @account = notification.from_account
+    #@me      = recipient
+    #@account = notification.from_account
 
-    return unless @me.user.functional?
+    #return unless @me.user.functional?
 
-    locale_for_account(@me) do
-      mail to: @me.user.email, subject: I18n.t('notification_mailer.follow.subject', name: @account.acct)
-    end
+    #locale_for_account(@me) do
+    #  mail to: @me.user.email, subject: I18n.t('notification_mailer.follow.subject', name: @account.acct)
+    #end
+    return
   end
 
   def favourite(recipient, notification)
-    @me      = recipient
-    @account = notification.from_account
-    @status  = notification.target_status
+    #@me      = recipient
+    #@account = notification.from_account
+    #@status  = notification.target_status
 
-    return unless @me.user.functional? && @status.present?
+    #return unless @me.user.functional? && @status.present?
 
-    locale_for_account(@me) do
-      thread_by_conversation(@status.conversation)
-      mail to: @me.user.email, subject: I18n.t('notification_mailer.favourite.subject', name: @account.acct)
-    end
+    #locale_for_account(@me) do
+    #  thread_by_conversation(@status.conversation)
+    #  mail to: @me.user.email, subject: I18n.t('notification_mailer.favourite.subject', name: @account.acct)
+    #end
+    return
   end
 
   def reblog(recipient, notification)
-    @me      = recipient
-    @account = notification.from_account
-    @status  = notification.target_status
+    #@me      = recipient
+    #@account = notification.from_account
+    #@status  = notification.target_status
 
-    return unless @me.user.functional? && @status.present?
+    #return unless @me.user.functional? && @status.present?
 
-    locale_for_account(@me) do
-      thread_by_conversation(@status.conversation)
-      mail to: @me.user.email, subject: I18n.t('notification_mailer.reblog.subject', name: @account.acct)
-    end
+    #locale_for_account(@me) do
+    #  thread_by_conversation(@status.conversation)
+    #  mail to: @me.user.email, subject: I18n.t('notification_mailer.reblog.subject', name: @account.acct)
+    #end
+    return
   end
 
   def follow_request(recipient, notification)
-    @me      = recipient
-    @account = notification.from_account
+    #@me      = recipient
+    #@account = notification.from_account
 
     # return unless @me.user.functional?
     return
@@ -68,21 +72,22 @@ class NotificationMailer < ApplicationMailer
   end
 
   def digest(recipient, **opts)
-    return unless recipient.user.functional?
+    #return unless recipient.user.functional?
 
-    @me                  = recipient
-    @since               = opts[:since] || [@me.user.last_emailed_at, (@me.user.current_sign_in_at + 1.day)].compact.max
-    @notifications_count = Notification.where(account: @me, activity_type: 'Mention').where('created_at > ?', @since).count
+    #@me                  = recipient
+    #@since               = opts[:since] || [@me.user.last_emailed_at, (@me.user.current_sign_in_at + 1.day)].compact.max
+    #@notifications_count = Notification.where(account: @me, activity_type: 'Mention').where('created_at > ?', @since).count
 
-    return if @notifications_count.zero?
+    #return if @notifications_count.zero?
 
-    @notifications = Notification.where(account: @me, activity_type: 'Mention').where('created_at > ?', @since).limit(40)
-    @follows_since = Notification.where(account: @me, activity_type: 'Follow').where('created_at > ?', @since).count
+    #@notifications = Notification.where(account: @me, activity_type: 'Mention').where('created_at > ?', @since).limit(40)
+    #@follows_since = Notification.where(account: @me, activity_type: 'Follow').where('created_at > ?', @since).count
 
-    locale_for_account(@me) do
-      mail to: @me.user.email,
-           subject: I18n.t(:subject, scope: [:notification_mailer, :digest], count: @notifications_count)
-    end
+    #locale_for_account(@me) do
+    #  mail to: @me.user.email,
+    #       subject: I18n.t(:subject, scope: [:notification_mailer, :digest], count: @notifications_count)
+    #end
+    return
   end
 
   private
