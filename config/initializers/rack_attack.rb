ee1ceacd41d3a28dd383d23560a528365436d0a5
rfg1024 <rfg1024@outlook.com>
2023-09-28 23:47:14 +0000

    Customization

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -74,7 +74,7 @@ class Rack::Attack
     req.authenticated_token_id if req.api_request?
   end
 
-  throttle('throttle_unauthenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_unauthenticated_api', limit: 60, period: 5.minutes) do |req|
     req.throttleable_remote_ip if req.api_request? && req.unauthenticated?
   end
 
@@ -105,15 +105,15 @@ class Rack::Attack
     req.authenticated_user_id if (req.post? && req.path.match?(API_DELETE_REBLOG_REGEX)) || (req.delete? && req.path.match?(API_DELETE_STATUS_REGEX))
   end
 
-  throttle('throttle_sign_up_attempts/ip', limit: 25, period: 5.minutes) do |req|
+  throttle('throttle_sign_up_attempts/ip', limit: 25, period: 50.minutes) do |req|
     req.throttleable_remote_ip if req.post? && req.path_matches?('/auth')
   end
 
-  throttle('throttle_password_resets/ip', limit: 25, period: 5.minutes) do |req|
+  throttle('throttle_password_resets/ip', limit: 25, period: 50.minutes) do |req|
     req.throttleable_remote_ip if req.post? && req.path_matches?('/auth/password')
   end
 
-  throttle('throttle_password_resets/email', limit: 5, period: 30.minutes) do |req|
+  throttle('throttle_password_resets/email', limit: 5, period: 300.minutes) do |req|
     req.params.dig('user', 'email').presence if req.post? && req.path_matches?('/auth/password')
   end
 
