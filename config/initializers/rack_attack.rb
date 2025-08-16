519417797630e351a0d1b3e692d78be77193bdd4
mastodon <mastodon@vmi2417066.contaboserver.net>
2025-08-16 21:17:37 +0200

    feat: customization

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -74,7 +74,7 @@ class Rack::Attack
     req.authenticated_token_id if req.api_request?
   end
 
-  throttle('throttle_unauthenticated_api', limit: 300, period: 5.minutes) do |req|
+  throttle('throttle_unauthenticated_api', limit: 60, period: 5.minutes) do |req|
     req.throttleable_remote_ip if req.api_request? && req.unauthenticated?
   end
 
@@ -94,7 +94,7 @@ class Rack::Attack
     req.authenticated_user_id if req.paging_request?
   end
 
-  throttle('throttle_unauthenticated_paging', limit: 300, period: 15.minutes) do |req|
+  throttle('throttle_unauthenticated_paging', limit: 60, period: 15.minutes) do |req|
     req.throttleable_remote_ip if req.paging_request? && req.unauthenticated?
   end
 
@@ -113,11 +113,11 @@ class Rack::Attack
     req.throttleable_remote_ip if req.post? && req.path_matches?('/auth')
   end
 
-  throttle('throttle_password_resets/ip', limit: 25, period: 5.minutes) do |req|
+  throttle('throttle_password_resets/ip', limit: 25, period: 25.minutes) do |req|
     req.throttleable_remote_ip if req.post? && req.path_matches?('/auth/password')
   end
 
-  throttle('throttle_password_resets/email', limit: 5, period: 30.minutes) do |req|
+  throttle('throttle_password_resets/email', limit: 5, period: 120.minutes) do |req|
     req.params.dig('user', 'email').presence if req.post? && req.path_matches?('/auth/password')
   end
 
